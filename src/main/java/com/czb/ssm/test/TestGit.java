7f79a9950befc53dda86eaf48acd03a74bd79f01
123czb <116644479+123czb@users.noreply.github.com>
2022-10-27 13:15:35 +0800

    Update TestGit.java

--- src/main/java/com/czb/ssm/test/TestGit.java
+++ src/main/java/com/czb/ssm/test/TestGit.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.Test;
 public class TestGit {
     @Test
     public void test1() {
-        System.out.println("This is the forth");
-        System.out.println("main");
-        System.out.println("hot-fix");
-        System.out.println("ssh");
+        System.out.println("pull");
     }
 }
